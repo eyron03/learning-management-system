d2ef8fb6e1e6719f006084167093567fe77f556a
eyron03 <aaronchristianarenas50@gmail.com>
2025-03-05 09:50:15 +0800

    Your commit message

--- components/navbar-home.tsx
+++ components/navbar-home.tsx
@@ -68,7 +68,7 @@ export function Navbar() {
     </header>
   )
 }
-ewewsasa
+ewewsasasasa
 interface MobileLinkProps extends React.ComponentPropsWithoutRef<"a"> {
   onOpenChange?: (open: boolean) => void
   children: React.ReactNode
